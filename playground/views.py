969f33956aaf9ac3b1a7d1bb39e21b333bb1c5b0
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-09-14 10:22:53 +0330

    add redis to project for caching

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,44 @@
 from django.core.mail import send_mail, mail_admins, EmailMessage, BadHeaderError
 from django.shortcuts import render
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 from templated_mail.mail import BaseEmailMessage
 from .tasks import notify_customers
+import requests
 
 
-def say_hello(request):
+"""
+classbase view
+"""
+class Helloview(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+          response = requests.get('https://httpbin.org/delay/2')
+          data= response.json()
+          return render(request, 'hello.html', {'name': 'Mahdi'})
+                
+        
+"""
+functional view
+"""
+# @cache_page(5 * 60)
+# def say_hello(request):
+#         response = requests.get('https://httpbin.org/delay/2')
+#         data= response.json()
+#         return render(request, 'hello.html', {'name': data})
+
+
+
+#---------cache data-----------
+# def say_hello(request):
+#     key = 'http_result'
+#     if cache.get(key) is None:
+#         response = requests.get('https://httpbin.org/delay/2')
+#         data= response.json()
+#         cache.set(key, data)
+#     return render(request, 'hello.html', {'name': cache.get(key)})
 #--------------------------------------------------------------=-
     # """sending email"""
     # try:
@@ -18,7 +52,11 @@ def say_hello(request):
     #     pass
 
 #-----------------------------------------------------------------
-
-    notify_customers.delay('Hello')
+    # """
+    #     celery usage
+    # """
+    # notify_customers.delay('Hello')
     
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # return render(request, 'hello.html', {'name': 'Mosh'})
+
+#-----------------------------------------------------------------
